7d2213d39150a32d5ad775ce6f12ad0183029fda
Gio Gottardi <ManuelGGottardi@gmail.com>
2015-08-24 16:33:37 -0500

    Filled in or created .travis.yml, MANIFEST.in, requirements-dev.txt, setup.py, and tox.ini

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-controlled-vocabularies',
+    version='1.0.0',
+    packages=find_packages(exclude=['tests*']),
+    description='',
+    long_description='See the home page for more information.',
+    include_package_data=True,
+    url='https://github.com/unt-libraries/django-controlled-vocabularies',
+    author='University of North Texas Libraries',
+    author_email='mark.phillips@unt.edu',
+    license='BSD',
+    keywords=['django', 'controlled', 'vocabularies', 'vocabulary'],
+    classifiers=[
+        'Natural Language :: English',
+        'Environment :: Web Environment',
+        'Framework :: Django :: 1.8',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ]
+)
