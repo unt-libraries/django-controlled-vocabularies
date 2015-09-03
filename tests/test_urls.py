09084b0f9647e302e8f6dd5d17a0793bf35210ea
Gio Gottardi <ManuelGGottardi@gmail.com>
2015-09-03 10:19:15 -0500

    Added a couple brief url tests.

--- tests/test_urls.py
+++ tests/test_urls.py
@@ -1,22 +1,27 @@
+from django.core.urlresolvers import resolve
+
+from controlled_vocabularies import views
+
+
 def test_vocabulary_list():
-    pass
+    assert resolve('/vocabularies/').func == views.vocabulary_list
 
 
 def test_verbose_vocabularies():
-    pass
+    assert resolve('/vocabularies/all-verbose/').func == views.verbose_vocabularies
 
 
 def test_about():
-    pass
+    assert resolve('/vocabularies/about/').func == views.about
 
 
 def test_all_vocabularies():
-    pass
+    assert resolve('/vocabularies/all/').func == views.all_vocabularies
 
 
 def test_term_list():
-    pass
+    assert resolve('/vocabularies/some_vocab_name/').func == views.term_list
 
 
 def test_vocabulary_file():
-    pass
+    assert resolve('/vocabularies/some_list_name/some_format/').func == views.vocabulary_file
