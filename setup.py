aabec69bb6f1c0fc10b9fcf8fc3dba17b2b397c1
Gio Gottardi <ManuelGGottardi@gmail.com>
2015-10-01 10:31:29 -0500

    Changed django 1.8 to django 1.6 in setup.py and added .cache to gitignore file.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     classifiers=[
         'Natural Language :: English',
         'Environment :: Web Environment',
-        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.6',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
